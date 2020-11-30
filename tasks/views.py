26b2bcac6e265ea5e46fe5576331611ecfd8bed3
Evandro Romeiro <evandrofr@al.insper.edu.br>
2020-11-30 17:27:11 -0300

    get teste 3

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+
 from tasks.serializer import TaskSerializer
 from tasks.models import Task
 
@@ -11,7 +12,6 @@ def index(request):
     return HttpResponse("Hello, world. You're at the tasks index.")
 
 def get_tasks(request):
-    # if request.method == 'GET':
-    #     serializer = TaskSerializer(Task.objects.all(), many=True)
-    #     return JsonResponse(serializer.data, safe=False)
-    return {'status': 200}
+    if request.method == 'GET':
+        serializer = TaskSerializer(Task.objects.all(), many=True)
+        return JsonResponse(serializer.data, safe=False)
